8d8dbcad17fbdba1ee3f1b7d0746602f375b7b3a
Cristian0808 <62268218+Cristian0808@users.noreply.github.com>
2022-08-15 12:04:14 -0300

    Update instalar-linux-na-tv-box.sh

--- instalar-linux-na-tv-box.sh
+++ instalar-linux-na-tv-box.sh
@@ -5,11 +5,11 @@ mkdir linux
 cd linux 
 tar -xvf ../ubuntu-base-22.04.1-base-armhf.tar.gz
 cd ..
-cat > .termux/boot/linux << "EOF"
+cat > /data/data/com.termux/files/home/.termux/boot/linux << "EOF"
 unset LD_PRELOAD
 proot -w /data/data/com.termux/files/home/linux -b /dev -b /sys -b /proc /init.sh
 EOF
-cat > linux/init.sh << "EOF"
+cat > /data/data/com.termux/files/home/linux/init.sh << "EOF"
 exec /etc/rc.local
 exec /bin/bash
 EOF
