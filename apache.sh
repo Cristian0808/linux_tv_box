9bfe61616b3ed139c342017541fdcc8028c3cb46
Cristian0808 <62268218+Cristian0808@users.noreply.github.com>
2022-09-19 08:48:10 -0300

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget https://dlcdn.apache.org/httpd/httpd-2.4.54.tar.gz
+tar -xvf httpd-2.4.54.tar.gz
 cd httpd-2.4.54
 ./configure --prefix=$(pwd)/apache --exec-prefix=$(pwd)/apache
 make -j2
